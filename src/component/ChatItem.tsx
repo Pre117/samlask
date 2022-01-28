644e290c541c86d4b81595819ad74fccdf8882ec
Pre117 <www2812414309@163.com>
2022-01-29 06:15:04 +0800

    完成消息列表样式开发

---
+++ src/component/ChatItem.tsx
@@ -0,0 +1,25 @@
+const ChatItem = (props: {
+    username: string
+    imgUrl: string
+    lastMessage: string
+    date: string
+}) => {
+    const { username, imgUrl, lastMessage, date } = props
+
+    return (
+        <div className="w-full h-16 flex">
+            <div className="pl-2 flex justify-center items-center">
+                <img className="w-12 h-12 mr-1 rounded-full" src={imgUrl} />
+            </div>
+            <div className="w-5/6 p-2 flex flex-col justify-between border-b">
+                <div className="flex justify-between tracking-wide">
+                    <div>{username}</div>
+                    <div className="text-sm text-gray-600">{date}</div>
+                </div>
+                <div className="text-sm text-gray-600  truncate">{lastMessage}</div>
+            </div>
+        </div>
+    )
+}
+
+export default ChatItem
--- src/pages/message/Message.tsx
+++ src/pages/message/Message.tsx
@@ -1,32 +1,55 @@
 import { useState } from 'react'
-import { io } from 'socket.io-client'
+import ChatItem from '../../component/ChatItem'
 import Header from '../../component/Header'
+import avatar from './getAvatar.jpg'
+
+const chatProps = {
+    username: '猫不理饺子',
+    imgUrl: avatar,
+    lastMessage: '这是最后！！欧打首付款，金阿奎龙卷风撒放假啦阿拉斯加疯狂拉师傅金坷垃',
+    date: '01-12'
+}
+
+const arr = new Array(20).fill(chatProps)
 
 const Message = () => {
     const [message, setMessage] = useState('')
 
-    const socket = io('http://localhost:9000/')
 
-    const onSubmit = () => {
-        console.log('message: ', message)
-        socket.emit('chat message', message)
-    }
+    // const socket = io('http://localhost:9000/')
 
-    socket.on('chat message', (msg: any) => {
-        console.log('transport message: ', msg)
-    })
+    // const onSubmit = () => {
+    //     console.log('message: ', message)
+    //     socket.emit('chat message', message)
+    // }
+
+    // socket.on('chat message', (msg: any) => {
+    //     console.log('transport message: ', msg)
+    // })
 
     return (
-        <div>
+        <div className="bg-gray-100">
             <Header />
-            我是消息页面
-            <input
-                type="text"
-                placeholder="请输入消息..."
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-            />
-            <button onClick={onSubmit}>提交</button>
+            <div className="w-full h-20 flex justify-around items-center mb-2 bg-white">
+                <div>通知</div>
+                <div>赞</div>
+                <div>回复</div>
+                <div>关注</div>
+            </div>
+            <div className='bg-white'>
+                {
+                    arr.map((item: any, index: number) => (
+                        <ChatItem key={index} {...item} />
+                    ))
+                }
+                {/* <input
+                    type="text"
+                    placeholder="请输入消息..."
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                />
+                <button onClick={onSubmit}>提交</button> */}
+            </div>
         </div>
     )
 }
