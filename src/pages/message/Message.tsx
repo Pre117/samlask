78d6172a195aa0d8b2d56d50f319d38a00cd7108
Pre117 <www2812414309@163.com>
2022-01-29 02:44:57 +0800

    初步完成基于socket.id的消息通讯

--- src/pages/message/Message.tsx
+++ src/pages/message/Message.tsx
@@ -1,10 +1,32 @@
-import Header from "../../component/Header"
+import { useState } from 'react'
+import { io } from 'socket.io-client'
+import Header from '../../component/Header'
 
 const Message = () => {
+    const [message, setMessage] = useState('')
+
+    const socket = io('http://localhost:9000/')
+
+    const onSubmit = () => {
+        console.log('message: ', message)
+        socket.emit('chat message', message)
+    }
+
+    socket.on('chat message', (msg: any) => {
+        console.log('transport message: ', msg)
+    })
+
     return (
         <div>
             <Header />
             我是消息页面
+            <input
+                type="text"
+                placeholder="请输入消息..."
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+            />
+            <button onClick={onSubmit}>提交</button>
         </div>
     )
 }
