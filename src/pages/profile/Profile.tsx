62c7860755fd3344074a894e4b1107098eaef67c
Pre117 <www2812414309@163.com>
2022-03-31 23:37:23 +0800

    完成部分样式

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -31,8 +31,29 @@ const Profile = () => {
     }, [])
 
     return (
-        <div>
+        <div className="bg-gray-100">
             <Header />
+            <div className="mt-8 p-6 bg-white flex flex-col items-center">
+                <div className="w-24 h-24">
+                    <img src={userInfo.avatar} className="rounded-full" />
+                </div>
+                <div className='text-lg my-4'>{userInfo.username}</div>
+                <div className='w-full flex justify-around'>
+                    <div>
+                        <div>0</div>
+                        <div className='text-sm text-gray-400'>关注</div>
+                    </div>
+                    <div>
+                        <div>0</div>
+                        <div className='text-sm text-gray-400'>关注者</div>
+                    </div>
+                    <div>
+                        <div>{userInfo.points}</div>
+                        <div className='text-sm text-gray-400'>积分</div>
+                    </div>
+                    <div className='w-20 h-8 border text-center'>设置</div>
+                </div>
+            </div>
             我是个人主页
             <div>用户名：{userInfo.username}</div>
             <div>积分：{userInfo.points}</div>
